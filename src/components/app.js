82a540a3ef8c20aec2ad182edc6ae72d7d63ab4e
oliverlai55 <oliverlai55@gmail.com>
2016-11-05 17:28:09 -0400

    Install Bootrap4

--- src/components/app.js
+++ src/components/app.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
+import Appbar from 'muicss/lib/react/appbar';
+import Button from 'muicss/lib/react/button';
+import Container from 'muicss/lib/react/container';
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
+//
+// let Appbar = mui.react.Appbar,
+//     Button = mui.react.Button,
+//     Container = mui.react.Container;
+
+// const data = {
+//   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//   datasets: [
+//     {
+//       label: 'My First dataset',
+//       fill: false,
+//       lineTension: 0.1,
+//       backgroundColor: 'rgba(75,192,192,0.4)',
+//       borderColor: 'rgba(75,192,192,1)',
+//       borderCapStyle: 'butt',
+//       borderDash: [],
+//       borderDashOffset: 0.0,
+//       borderJoinStyle: 'miter',
+//       pointBorderColor: 'rgba(75,192,192,1)',
+//       pointBackgroundColor: '#fff',
+//       pointBorderWidth: 1,
+//       pointHoverRadius: 5,
+//       pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+//       pointHoverBorderColor: 'rgba(220,220,220,1)',
+//       pointHoverBorderWidth: 2,
+//       pointRadius: 1,
+//       pointHitRadius: 10,
+//       data: [65, 59, 80, 81, 56, 55, 40]
+//     }
+//   ]
+// };
 
 export default class App extends Component {
   // displayName: 'LineExample',
@@ -34,9 +42,10 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h2>Line Example</h2>
-        <Line data={data} />
-
+          <Appbar></Appbar>
+                  <Container>
+                    <Button color="primary">button</Button>
+                  </Container>
       </div>
     );
   }
